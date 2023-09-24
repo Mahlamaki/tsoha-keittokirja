970374dd63a88e5412de74ce17493b6eae4f1611
Susanna Mahlamäki <liisa.mahlamaki@helsinki.fi>
2023-09-24 22:03:42 +0300

    Create first version

---
+++ routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import redirect, render_template, request, session
+
+
+@app.route("/")
+def index():
+    return render_template("index.html") 
+
+@app.route("/luo-uusi-resepti", methods=["POST"])
+def new():
+    if request.method= == "POST":
+        return render_template("new_recipe.html")
