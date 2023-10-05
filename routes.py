d642366ab078a9cf44279fafc46e1e2dfa6f3866
Susanna Mahlamäki <liisa.mahlamaki@helsinki.fi>
2023-10-05 12:45:28 +0300

    Updating routes.py

--- routes.py
+++ routes.py
@@ -1,12 +1,24 @@
+from sqlalchemy.sql import text
 from app import app
 from flask import redirect, render_template, request, session
-
+from db import db
 
 @app.route("/")
 def index():
     return render_template("index.html") 
 
-#@app.route("/luo-uusi-resepti", methods=["POST"])
-#def new():
-#    if request.method= == "POST":
-#        return render_template("new_recipe.html")
+
+@app.route("/luo-uusi-resepti")
+def new_recipe():
+    return render_template("new_recipe.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    name = request.form["title"]
+    recipe =request.form["message"]
+    category = request.form["category"]
+    sql = f"INSERT INTO recipe (category_id, name, content) VALUES ('{category}','{name}', '{recipe}')"
+    db.session.execute(text(sql))
+    db.session.commit()
+    return redirect("/")
+
