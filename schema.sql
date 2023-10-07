364f060d85a74e24a13a466f55b3aa878d81076a
Mahlamaki <117199066+Mahlamaki@users.noreply.github.com>
2023-10-07 18:48:14 +0300

    Update schema.sql, adding tables users and favourites

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
 CREATE TABLE recipe(id SERIAL PRIMARY KEY, category_id INTEGER REFERENCES category, name TEXT, content TEXT);
 
-CREATE TABLE category(id SERIAL PRIMARY KEY,name TEXT);
+CREATE TABLE category(id SERIAL PRIMARY KEY, name TEXT);
 
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+
+CREATE TABLE favourites(id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, recipe_id INTEGER REFERENCES recipe);
