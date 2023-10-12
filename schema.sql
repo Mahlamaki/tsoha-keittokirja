d4f7fbc8b20ce381ca9ccf7535e63469ee8c55b8
Susanna Mahlamäki <liisa.mahlamaki@helsinki.fi>
2023-10-12 20:56:57 +0300

    Making username unique

--- schema.sql
+++ schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE recipe(id SERIAL PRIMARY KEY, category_id INTEGER REFERENCES catego
 
 CREATE TABLE category(id SERIAL PRIMARY KEY, name TEXT);
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 
 CREATE TABLE favourites(id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, recipe_id INTEGER REFERENCES recipe);
